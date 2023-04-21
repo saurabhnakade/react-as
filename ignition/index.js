ce93a14f404db01ab10ced086f1be65133674051
Saurabh Nakade <saurabh@sma.local>
2023-04-21 18:54:11 +0530

    inception

---
+++ ignition/index.js
@@ -0,0 +1,23 @@
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", { key: "1", id: "heading1" }, "Hello World"),
+        React.createElement("h1", { key: "2", id: "heading2" }, "In React JS"),
+    ])
+);
+
+const parent2 = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { key: "div1", id: "child" }, [
+        React.createElement("h1", { key: "1", id: "heading1" }, "Hello World"),
+        React.createElement("h1", { key: "2", id: "heading2" }, "In React JS"),
+    ]),
+    React.createElement("div", { key: "div2", id: "child" }, [
+        React.createElement("h1", { key: "1", id: "heading1" }, "Hello World"),
+        React.createElement("h1", { key: "2", id: "heading2" }, "In React JS"),
+    ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent2);
